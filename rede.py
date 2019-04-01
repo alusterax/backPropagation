0389306a3a463954c4a5c5659e652ae0a09a81a5
alusterax <38334986+alusterax@users.noreply.github.com>
2019-04-01 08:08:02 -0300

    Update rede.py

--- rede.py
+++ rede.py
@@ -1,45 +1,61 @@
 import math
 import numpy as np
-#TO-DO: 
-# Criar matriz input e outputs (e fazer abrir do csv)
-# Completar função de propagação (de preferencia que funciona de X para Z e Z para Y)
-# Criar função para atribuir pesos aleatórios (de -1 a 1)
-# Definir quantos neuronios na camada intermediária (Max sugeriu começar com 100)
-#___________
+import pandas as pd
 
-#matriz Input [728] = Recebe csv;
-#matriz Output[10 ] = Recebe csv;
+qtInter = 100
+v = np.random.uniform(-1,1,(784,qtInter))
+w = np.random.uniform(-1,1,(qtInter,10))
+df = pd.read_csv('csvTest.csv')
+delK = np.zeros(10)
+delW = np.zeros((100,10))
+inDel = np.zeros(100)
+DeltaDelta = np.zeros(100)
 
-#Legendas: 
-# X - Input, Z - Intermediária, Y - Output, target - Valor alvo de saída desejado, e que é fornecido 
-# velAprend - Velocidade de aprendizado, erro - diferença entre output e target
-# deltaK - Fator de distrib. de erro Z <- Y (backpropagation,ajuste de peso)
-# deltaJ - Fator de distrib. de erro X <- Z     - || -
-
-X = np.array([ [-1],[-1],[-1],[-1] ])
-V = np.array([ [0.3, -0.4],
-               [0.1,  0.6],
-               [-0.2, 0.1],
-               [0,   -0.5] ])
-Z = np.array([[0],[0]])   #vetor 100            
-def propag() :
-    for i in range(3):
-        for j in range (1):
-           Z[j] +=   X[i] * V[i][j]
-    #foreach in Input xi, Pesos vij, Xi*Vij;
+def propag (inp, pesos) :
+    soma =0
+    qtInput = len(inp)
+    inX = np.zeros(np.size(pesos,axis=1))
+    for j in range (np.size(pesos,axis=1)):
+        for i in range (qtInput):
+            soma +=(inp[i]/2550) * v[i][j]
+        inX[j] = soma
+        soma =0
+    return inX
+    
 def funcAtivacao(x) :
-    return 1/(1+math.exp(-x))
+    Ze = np.zeros(len(x))
+    for i in range(len(x)):
+        Ze[i] = (2/1+ np.exp(-x[i])) -1
+    return Ze
+  
 def deltaK(targetK,Yk,YinK) :
-    erro = targetK - Yk
-    deriv = YinK * (1-YinK)
-    return erro*deriv
-def deltaWjk(velAprend,delK,Zj) :
-    return velAprend * delK * Zj
+        erro = targetK - Yk
+        deriv = YinK * (1 - YinK)
+        return erro * deriv
+     
+def deltaW(alpha,dK,Ze):
+    for j in range(10):
+        for i in range(qtInter):
+            delW[i][j] = alpha * delK[j] * Z[i]
 
+#separa csv em dois arrays, a = input, target = target.
+for row in df.itertuples(index=False):
+    a = list(row)
+    target = a.pop(0)
 
-print (deltaK(-1,0.459,-0.164))
-print (deltaK(-1,0.629,-0.529))
-print (deltaWjk(0.2,-0.362,0.59))
-print (X)
+#forward
+inZ = propag(a,v)
 
-propag()
+Z = funcAtivacao(inZ)
+
+inY = propag(Z,w)
+
+Y = funcAtivacao(inY)
+
+#calculo de erro
+for i in range (10) :
+    delK[i] = deltaK(1,Y[i],inY[i])
+#backpropagation    
+deltaW(0.2,delK,Z)
+
+#continua
